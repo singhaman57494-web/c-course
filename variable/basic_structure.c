b4dfb5f8c4e55c85ecc5dad6af01bdb46228cbe8
singhaman57494-web <singhaman57494@gmail.com>
2026-07-26 17:12:39 +0530

    Add files via upload

---
+++ variable/basic_structure.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // required for printf()
+
+int main() {
+    // printf print text to the screen
+    // \n moves the cursor to a new line 
+    printf("Hello, World!\n"); 
+    
+    return 0; // signals that the program ran successfully
+}
---
+++ variable/rules.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/*
+. variable must start with alphabet or underscore.
+EX:- Add, add, _add, _sum
+
+. you can use digit, ulphabet and underscore.
+EX:- add123, Ad32, _sum123
+
+. no white space is allowed.
+EX:- first value, second value, first value123
+
+. a variable should not be keywoards.
+*/
