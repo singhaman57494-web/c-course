90d9dc8acfac9f40522e18c5ab8f296e7e68ba82
singhaman57494-web <singhaman57494@gmail.com>
2026-07-28 19:10:15 +0530

    Added input output

---
+++ input output/input.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main ()
+{
+    int number;
+
+    printf("enter your number : ");
+    scanf(" %d", &number); // takes integer input from user
+
+    printf("your number : %d\n", number);
+
+    return 0;
+}
