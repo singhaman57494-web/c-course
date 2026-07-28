f6a7a2a81162cbde82dbd31529bfd626efd34c31
singhaman57494-web <singhaman57494@gmail.com>
2026-07-28 17:32:39 +0530

    added math problem solving

--- math_problem_solving/33_happy_number.c
+++ math_problem_solving/33_happy_number.c
@@ -9,28 +9,29 @@ int main ()
     printf("enter the number :");
     scanf("%d", &n);
 
+    int visited[1000] = {0};
+
     while(n != 1)
     {
+        if(visited[n] == 1)
+        {
+            printf("not happy number");
+            return 0;
+        }
+        visited[n] = 1;
+
         int sum = 0;
+
         for(int i = n; i > 0; i = i / 10)
         {
             int digits = i % 10;
 
             sum = sum + digits * digits;
         }
-    n = sum;
+        n = sum;
     }
-    if(n == 1)
-    {
-        printf("happy number");
-    }
-    else
-    {
-        if(n )
-        {
-            
-        }
-    }
+
+    printf("happy number");
 
     return 0;
 }
