ad2f667abcc24a5f57b3e72565e26c4abe77cc7d
singhaman57494-web <singhaman57494@gmail.com>
2026-08-07 15:33:24 +0530

    added my code

---
+++ string/06_strcat.c
@@ -0,0 +1,14 @@
+//                                concatination in c 
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char name1 [] = "aman";
+    char username[] = "chauhan";
+
+    puts(strcat(name1 , username));
+    return 0;
+}
