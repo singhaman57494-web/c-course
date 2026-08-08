b7c1266f926aaadfc0b4191ae8b2d6a68c3059d1
singhaman57494-web <singhaman57494@gmail.com>
2026-08-08 13:47:14 +0530

    Added my code

---
+++ structures/01_introduction_structture.c
@@ -0,0 +1,27 @@
+//                                 structure in c programming 
+
+/*
+question 1 :- what is a structure in c 
+
+1. structures are user defined data type in c.
+
+2. using structure allows us to combine data of different type together .
+
+3. it is used to create a complex data type which contains diverse information .
+
+4. they are very similar to array but structure can store data and any type , which s prectically
+   more useful.
+   
+
+
+
+//                            accessing structure members
+
+1. array element are accessed using the subscript variable.
+
+2. in a similar fashion, structure members are accessed using dot[.] operator.
+
+3. (.) is called as "structure member operator ".
+
+4. to access the member of the structure, we use this operator in between "structure name " & "member name ".
+*/
