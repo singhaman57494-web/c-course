d561c65c046b962525bf44993d6cf0d87b731213
singhaman57494-web <singhaman57494@gmail.com>
2026-08-14 23:54:03 +0530

    added c code

--- datatype/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-# include <stdio.h>
-
-int main ()
-{
-    printf("%lu", sizeof(float));
-    return 0;
-}
--- variable/basic_structure.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> // required for printf()
-
-int main() {
-    // printf print text to the screen
-    // \n moves the cursor to a new line 
-    printf("Hello, World!\n"); 
-    
-    return 0; // signals that the program ran successfully
-}
