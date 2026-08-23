c3fc1522110ee01b6bcc8959158874117e6060ef
singhaman57494-web <singhaman57494@gmail.com>
2026-08-23 18:48:21 +0530

    added this code

---
+++ projects/mini_quiz_game.c/mina.c
@@ -0,0 +1,355 @@
+#include <stdio.h>
+
+void Quiz()
+{
+    char choice;
+
+    printf("\nQuestion 1 :- which function is used to make formatted input in c?\n");
+    printf("(A) scanf()\n");
+    printf("(B) printf()\n");
+    printf("(C) input()\n");
+    printf("(D) read()\n");
+
+    printf("enter the choice(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'A')
+    {
+        printf("write Answer\n");
+    }
+    else
+    {
+        printf("wrong Answer\n ");
+    }
+
+    printf("\nQuestion 2 :- which language are we currently using? \n");
+    printf("(A) python\n");
+    printf("(B) c\n");
+    printf("(C) java\n");
+    printf("(D) javascript\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 3 :- which symbol is used to end a C statement? \n");
+    printf("(A) :\n");
+    printf("(B) ;\n");
+    printf("(C) .\n");
+    printf("(D) ,\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 4 :- which function is used to display output in C? \n");
+    printf("(A) print()\n");
+    printf("(B) printf()\n");
+    printf("(C) display()\n");
+    printf("(D) count()\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 5 :- which datatype is used to store a single character? \n");
+    printf("(A) string\n");
+    printf("(B) char\n");
+    printf("(C) character\n");
+    printf("(D) taxt\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 6 :- which operator is used to get the remainder of a division? \n");
+    printf("(A) /\n");
+    printf("(B) //\n");
+    printf("(C) %%\n");
+    printf("(D) rem\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 7 :- which loops is guaranteed to execute at least once? \n");
+    printf("(A) for\n");
+    printf("(B) while\n");
+    printf("(C) do_while\n");
+    printf("(D) None\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 8 :- Which keyword is used to return a value from a function ? \n");
+    printf("(A) break\n");
+    printf("(B) return\n");
+    printf("(C) exit\n");
+    printf("(D) send\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 9 :- Array indexing in C starts from ? \n");
+    printf("(A) 0\n");
+    printf("(B) 1\n");
+    printf("(C) -1\n");
+    printf("(D) 2\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'A')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 10 :-  Which is the correct way to declare an integer variable?\n");
+    printf("(A) integer x\n");
+    printf("(B) x int\n");
+    printf("(C) int x\n");
+    printf("(D) num x\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 11 :- Which keyword is used to make a variable constant? \n");
+    printf("(A) fixed\n");
+    printf("(B) constant\n");
+    printf("(C) const\n");
+    printf("(D) immutable\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 12 :- Which operator is used to compare two values for equality? \n");
+    printf("(A) =\n");
+    printf("(B) ==\n");
+    printf("(C) ===\n");
+    printf("(D) !=\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 13 :- What does break do inside a loop? \n");
+    printf("(A) restarts the loop \n");
+    printf("(B) skips one iteration\n");
+    printf("(C) exits the loop\n");
+    printf("(D) stop the program permanently");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 14 :- Which keyword is used for decision making? \n");
+    printf("(A) if\n");
+    printf("(B) chek\n");
+    printf("(C) condition\n");
+    printf("(D) when\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'A')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 15 :- Which symbol is used to access the address of a variable? \n");
+    printf("(A) *\n");
+    printf("(B) &\n");
+    printf("(C) #\n");
+    printf("(D) @\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 16 :- Which symbol is used to dereference a pointer? \n");
+    printf("(A) &\n");
+    printf("(B) *\n");
+    printf("(C) %%\n");
+    printf("(D) ->\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'B')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 17 :- Which header file is commonly used for printf() and scanf()? \n");
+    printf("(A) <string.h\n");
+    printf("(B) <math.h>\n");
+    printf("(C) <stdio.h\n");
+    printf("(D) <stdlib.h>\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 18 :- What does \\0 represent in a C string? \n");
+    printf("(A) space\n");
+    printf("(B) new line\n");
+    printf("(C) null character\n");
+    printf("(D) zero as a number\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'C')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 19 :- Which keyword is used to define a structure? \n");
+    printf("(A) struct\n");
+    printf("(B) structure\n");
+    printf("(C) record\n");
+    printf("(D) object\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'A')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+    printf("\nQuestion 20 :- Which of these is not a valid C data type? \n");
+    printf("(A) int\n");
+    printf("(B) float\n");
+    printf("(C) char\n");
+    printf("(D) string\n");
+
+    printf("enter the guess(A-D) ");
+    scanf(" %c", &choice);
+
+    if (choice == 'D')
+    {
+        printf("write Answer");
+    }
+    else
+    {
+        printf("wrong Answer ");
+    }
+}
+
+int main()
+{
+    Quiz();
+
+    return 0;
+}
